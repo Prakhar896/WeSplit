786bb5840a262b6948c6a94af7c4a4c4729e2103
Prakhar Trivedi <prakh0706@outlook.com>
2021-11-11 00:57:36 +0800

    Go through 5/11 tutorials in the Introduction topic of Day 16.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    @State private var tapCount = 0
+    @State private var name = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Form {
+//            Section {
+//                Text("Hello, world!")
+//            }
+//            Section {
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//            }
+//        }
+//        NavigationView {
+//            Form {
+//                Section {
+//                    Text("Hello world!")
+//                }
+//            }
+//            .navigationTitle("My worlds")
+//            .navigationBarTitleDisplayMode(.inline)
+//        }
+//        Button("Tap count: \(tapCount)") {
+//            tapCount += 1
+//        }
+        Form {
+            TextField("Enter your name", text: $name)
+            Text("Hello world! \(name)")
+        }
     }
 }
 
