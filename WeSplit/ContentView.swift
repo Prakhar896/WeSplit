38af90f42292aa49236f571b3918fe76e2362b6a
Prakhar Trivedi <prakh0706@outlook.com>
2021-11-11 13:02:22 +0800

    Just finished WeSplit project, now gonna do the extra challenges that are optional

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,7 +10,26 @@ import SwiftUI
 struct ContentView: View {
     
 //    @State private var tapCount = 0
-    @State private var name = ""
+//    @State private var name = ""
+//    let students = ["Harry", "Hermione", "Ron"]
+//    @State private var selectedStudent = "Harry"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        //calculate total per person
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        var tipAmount = tipSelection / 100 * checkAmount
+        var total = checkAmount + tipAmount
+        var split = total / peopleCount
+        return split
+    }
     
     var body: some View {
 //        Form {
@@ -22,6 +41,8 @@ struct ContentView: View {
 //                Text("Hello, world!")
 //            }
 //        }
+        
+        
 //        NavigationView {
 //            Form {
 //                Section {
@@ -31,12 +52,77 @@ struct ContentView: View {
 //            .navigationTitle("My worlds")
 //            .navigationBarTitleDisplayMode(.inline)
 //        }
+        
+        
 //        Button("Tap count: \(tapCount)") {
 //            tapCount += 1
 //        }
-        Form {
-            TextField("Enter your name", text: $name)
-            Text("Hello world! \(name)")
+        
+        
+//        Form {
+//            TextField("Enter your name", text: $name)
+//            Text("Hello world! \(name)")
+//        }
+        
+//        Form {
+//            ForEach(0..<100) {
+//                Text("Row \($0)")
+//            }
+//        }
+        
+//        NavigationView {
+//            Form {
+//                Picker("Select your student", selection: $selectedStudent) {
+//                    ForEach(students, id: \.self) {
+//                        Text($0)
+//                    }
+//                }
+//            }
+//        }
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .padding()
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0)")
+                        }
+                    }
+                } header: {
+                    Text("Payment Amount")
+                }
+                
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency( code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Amount Per Person")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button ("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
